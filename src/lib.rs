4657d566efe83ff544f980e80a7f7f5797d5f90b
Patrick Barrett <patrick@psbarrett.com>
2018-01-27 16:32:10 -0600

    use error-chain

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+#![recursion_limit = "1024"]
+#[macro_use]
+extern crate error_chain;
+
 extern crate reqwest;
 
 extern crate serde;
@@ -6,3 +10,9 @@ extern crate serde_derive;
 extern crate serde_json;
 
 pub mod site;
+
+pub mod errors {
+    // Create the Error, ErrorKind, ResultExt, and Result types
+    error_chain!{}
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,36 @@
+#![recursion_limit = "1024"]
+#[macro_use]
+extern crate error_chain;
 extern crate neo;
 extern crate reqwest;
 
+// Note that this is different than the errors module in lib.rs
+mod errors {
+    error_chain!{}
+}
+use errors::*;
+
 fn main() {
+    if let Err(ref e) = run() {
+        use std::io::Write;
+        let stderr = &mut ::std::io::stderr();
+        let errmsg = "Error writing to stderr";
+
+        writeln!(stderr, "error: {}", e).expect(errmsg);
+
+        for e in e.iter().skip(1) {
+            writeln!(stderr, "caused by: {}", e).expect(errmsg);
+        }
+
+        if let Some(backtrace) = e.backtrace() {
+            writeln!(stderr, "backtrace: {:?}", backtrace).expect(errmsg);
+        }
+
+        ::std::process::exit(1);
+    }
+}
+
+fn run() -> Result<()> {
     let site = neo::site::Site::new("psbarrett".to_string(),
                                     "T[x1O\"qnGpr\"a>wAw\"U_Wkziv@O'nc\
                                      rc'yu)S7Cq-PRogy?Z46JcGp!dZ,)WRYOc"
@@ -19,4 +48,6 @@ fn main() {
 
     site.upload("test.html".to_owned(), "./testfile.html".into()).expect("upload failed");
     site.delete(vec!["test.html".to_owned()]).expect("delete failed");
+
+    Ok(())
 }
