12b82e4e1e54cc1f75bafc98fcf5c1fda4e1ebb4
Patrick Barrett <patrick@psbarrett.com>
2018-01-27 18:22:16 -0600

    upload siteinfo file on test

--- tests/integration.rs
+++ tests/integration.rs
@@ -4,17 +4,17 @@ extern crate dotenv;
 use dotenv::dotenv;
 use std::env;
 
-fn setup() {
+fn setup() -> neo::Site {
     dotenv().ok();
+
+    neo::Site::new(env::var("TEST_SITE").expect("TEST_SITE"),
+                   env::var("TEST_PASSWORD").expect("TEST_PASSWORD"),
+                   None)
 }
 
 #[test]
 fn full(){
-    setup();
-
-    let site = neo::Site::new(env::var("TEST_SITE").expect("TEST_SITE"),
-                              env::var("TEST_PASSWORD").expect("TEST_PASSWORD"),
-                              None);
+    let site = setup();
 
     let info = site.info().expect("info falied");
     println!("Site: {}", info.sitename);
@@ -27,4 +27,11 @@ fn full(){
 
     site.upload("test.html".to_owned(), "tests/assets/testfile.html".into()).expect("upload failed");
     site.delete(vec!["test.html".to_owned()]).expect("delete failed");
+}
+
+#[test]
+fn upload_site_info(){
+    let site = setup();
+
+    site.upload("index.html".to_owned(), "tests/assets/siteinfo.html".into()).expect("upload failed");
 }
